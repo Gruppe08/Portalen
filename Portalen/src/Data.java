a84b40458edaab1ab8d3f9699c9a4537cefd2670
BiancaJuul <biancajuul-hansen@biancas-macbook.hhk.dk>
2015-11-10 10:30:05 +0100

    Github Bianca Test

--- Portalen/src/Data.java
+++ Portalen/src/Data.java
@@ -10,7 +10,7 @@
 			events.add(new Event("Pop Musik " , "Justin Bieber ", "Portalens Teatersal " , "2/2/2016"));
 
 			bruger = new ArrayList<EventBruger>();
-			bruger.add(new EventBruger("William Karberg", "123abc", events));
+			bruger.add(new EventBruger("William Karberg12", "123abc", events));
 			bruger.add(new EventBruger("Simon Ottosen", "123abc", events));
 			bruger.add(new EventBruger("Bianca Juul-Hansen", "123abc", events));
 			bruger.add(new EventBruger("Peter Wynn Vuiff", "123abc", events));
