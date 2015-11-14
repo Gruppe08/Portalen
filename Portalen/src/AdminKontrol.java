4b83db7e26567728e0a248ade7814f5a6a6a10c9
williamkarberg <william@karberg.dk>
2015-11-14 18:58:13 +0100

    Data & AdminKontrol comments

--- Portalen/src/AdminKontrol.java
+++ Portalen/src/AdminKontrol.java
@@ -9,9 +9,12 @@ public class AdminKontrol {
 		this.input = input;
 		this.db = db;
 	}
-	public void listOfBrugere() {
+//Her udskives liste af eksisterende brugere (der bliver brugt en if/else metode)	
+	public void listOfBrugere() {	
+//Hvis der er ingen eksisterende brugere udskrives dette:
 		if (db.getBruger().size() == 0) {
 			System.out.println("Der er ingen brugere");
+//Her hentes der information om eksisterende brugere, som derefter udskrives
 		} else {
 			System.out.println("Her er alle eksisterende brugere: ");
 			for (Admin bruger : db.getBruger()) {
@@ -21,21 +24,26 @@ public class AdminKontrol {
 			
 		}
 		}
+//Her bliver muligt at tilføje ny bruger, med nyt password og nyt brugernavn
 	public void addBruger() {
 		System.out.println("Brugernavn: ");
 		String username = input.next();
 		System.out.println("Password: ");
 		int password = input.nextInt();
+//Her henter den information fra Bruger klassen og laver event=null
 		Bruger bruger = new Bruger(username, password, null);
 		db.getBruger().add(bruger);
 
 		
 	}
+//Her slettes der en valgfri af de oprettet bruger
 	public void deleteBruger() {
+//Her bliver der hentet information om alle eksisterende brugere
 		for (Admin bruger : db.getBruger()) {
 			System.out.println(db.getBruger().indexOf(bruger) + ": " + bruger.getUsername() + ", "
 					+ bruger.getPassword());
 		}
+//Her vælger adminen hvilken bruger der skal slettes
 		System.out.print("Hvilken bruger skal slettes? ");
 		db.getBruger().remove(input.nextInt());
 
--- Portalen/src/Data.java
+++ Portalen/src/Data.java
@@ -9,7 +9,8 @@ public class Data {
 	private Scanner input;
 
 	public void generateUsers() {
-
+//	//Her vises alle arrangementer som er oprettet på forhånd, 
+//	hvor de bliver fordelt i kategorierne: Genre, kunstner, lokale og dato
 		events = new ArrayList<Event>();
 		events.add(new Event("Musik", "Justin Bieber", "Portalens Teatersal", "10/12/2015", 10));
 		events.add(new Event("Musik", "Adele", "Portalens Teatersal", "05/07/2020", 10));
@@ -19,7 +20,8 @@ public class Data {
 		events.add(new Event("Scenekunst", "Snedronningen", "Mødelokale 1", "05/06/2006", 10));
 		events.add(new Event("Børneunderholdning", "Trylleshow", "Foyersalen (Portalens Biograf)", "05/06/2053", 10));
 		events.add(new Event("Børneunderholdning", "William Karberg", "Foyersalen (Portalens Biograf)", "26/02/2013", 10));
-		
+
+//Her vises alle brugere og admins, hvor man ser deres brugernavn (Sring) og passwords(Int)	
 		brugere = new ArrayList<Admin>();
 		brugere.add(new Bruger("a", 1, events));
 		brugere.add(new Bruger("William Karberg", 1234, events));
@@ -29,26 +31,27 @@ public class Data {
 		brugere.add(new Admin("b", 2));
 
 	}
-
+//Her ses login systemet
 	public boolean login() {
 
 		String username;
 		int password;
 
 		input = new Scanner(System.in);
-
+//Her skal man indtaste brugernavn og password
 		System.out.println("Indtast brugernavn for at logge ind: ");
 		username = input.nextLine();
 		System.out.println("Skriv dit password: ");
 		password = input.nextInt();
 
 		for (Admin bruger : brugere) {
-			
+//Her ses om brugernavn og password matcher, hvis ja så logger man ind 	
 			if (bruger.getUsername().equals(username) && bruger.getPassword() == password) {
 				setCurrentBruger(bruger);
 				return true;
 			}
 		}
+//Hvis nej så bliver dette udskrevet
 		System.out.println("Forkert brugernavn eller kode, prøv igen\n");
 		StartMenu program = new StartMenu();
 		program.run();
@@ -56,7 +59,7 @@ public class Data {
 
 	}
 		
-
+//Getters & Setters
 	public Admin getCurrentBruger() {
 		return currentBruger;
 	}
