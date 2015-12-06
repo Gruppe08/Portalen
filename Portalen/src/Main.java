8c869e9113da5ccef1d59450d590b1bb621610eb
simonottosen <simnik123@gmail.com>
2015-12-06 17:29:47 +0100

    Fjernet comment

--- Portalen/src/Main.java
+++ Portalen/src/Main.java
@@ -6,7 +6,6 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		
 		StartMenu program = new StartMenu();	//Her starter vi StartMenu.java op
